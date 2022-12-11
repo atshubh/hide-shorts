31cb8ff4744efa6a080104e48d8f63f616d0af61
Shubhanshu Aggarwal <shubhanshucodes@gmail.com>
2022-12-11 21:11:17 +0000

    feat: added code to hide shorts links and videos

--- src/scripts/hide-shorts.js
+++ src/scripts/hide-shorts.js
@@ -1,35 +1,42 @@
-onUrlChange()
+let lastUrl = location.href;
 
-let lastUrl = location.href; 
-new MutationObserver(() => {
+new MutationObserver((mutationList) => {
   const url = location.href;
+  console.info(url, lastUrl);
   if (url !== lastUrl) {
     lastUrl = url;
-    onUrlChange();
+    removeShorts();
   }
-}).observe(document, {subtree: true, childList: true});
 
-function onUrlChange() {
-    const shortsElementsToHide = [
-        document.querySelector('ytd-guide-entry-renderer:has([title=\"Shorts\"])'),
-        document.querySelector('ytd-mini-guide-entry-renderer:has([title=\"Shorts\"])')
-    ]
-    
-    
-    shortsElementsToHide.forEach((element) => {
-        if(element) {
-            element.style.display = "none"
-        }
-    })
-    
-    const shortsVideos = document.querySelectorAll('ytd-grid-video-renderer:has(a[href^="/shorts/"])')
-    
-    if(shortsVideos) {
-        shortsVideos.forEach((shortsVideo) => {
-            shortsVideo.style.display = "none"
-        })
+  if (mutationList.some((mutation) => mutation.type === "childList")) {
+    removeShorts();
+  }
+}).observe(document, {
+  subtree: true,
+  childList: true,
+});
+
+function removeShorts() {
+  const shortsElementsToHide = [
+    document.querySelector('ytd-guide-entry-renderer:has([title="Shorts"])'),
+    document.querySelector(
+      'ytd-mini-guide-entry-renderer:has([title="Shorts"])'
+    ),
+  ];
+
+  shortsElementsToHide.forEach((element) => {
+    if (element) {
+      element.style.display = "none";
     }
+  });
+
+  const shortsVideos = document.querySelectorAll(
+    'ytd-grid-video-renderer:has(a[href^="/shorts/"])'
+  );
+
+  if (shortsVideos) {
+    shortsVideos.forEach((shortsVideo) => {
+      shortsVideo.style.display = "none";
+    });
+  }
 }
-
-
-
